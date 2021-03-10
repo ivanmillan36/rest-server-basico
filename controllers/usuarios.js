10b63b336e7b58a9844f521c8da54eb1c371290a
ivan millan <ivanmillan36@gmail.com>
2021-03-10 20:51:10 -0300

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,52 @@
+const { response, request } = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+
+    const {q, nombre='No name', apikey } = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        nombre,
+        apikey
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const { id } = req.params;
+
+    res.status(500).json({
+        msg: 'put API - controlador',
+        id
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const {nombre, edad} = req.body;
+
+    res.json({
+        msg: 'post API - controlador',
+        nombre, edad
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - controlador'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosPatch,
+    usuariosDelete
+}
